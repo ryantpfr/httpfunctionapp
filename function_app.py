d48468330a78667de74f06acc47b212478068ebd
ryan toepfer <ryantpfr@gmail.com>
2025-03-10 10:53:13 -0500

    initial commit

---
+++ function_app.py
@@ -0,0 +1,14 @@
+import azure.functions as func
+import logging
+import os
+import requests
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    text = requests.get("https://www.google.com").text
+
+    return func.HttpResponse(text)
