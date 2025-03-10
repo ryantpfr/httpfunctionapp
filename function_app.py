4a71869a9d6944d3146257c0dcc10df802452de8
ryan toepfer <ryantpfr@gmail.com>
2025-03-10 11:25:16 -0500

    readd requests

--- function_app.py
+++ function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 import logging
 import os
-# import requests
+import requests
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -9,7 +9,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # text = requests.get("https://www.google.com").text
-    text = "dsdsdsd"
+    text = requests.get("https://www.google.com").text
+    # text = "dsdsdsd"
 
     return func.HttpResponse(text)
